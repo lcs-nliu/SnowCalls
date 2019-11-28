95ce1c9710552fec9ac6f359726e40712d0dd5b2
Nicole Liu <nicoleliu_@outlook.com>
2019-11-27 22:45:30 -0500

    Finished switch statement for letters uppercase

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     // Mark: Properties
     @IBOutlet weak var inputText: UITextView!
     @IBOutlet weak var outputText: UITextView!
-    
+    var builtNumberFromLoop = ""
     // Mark: Methods
     
     override func viewDidLoad() {
@@ -23,12 +23,54 @@ class ViewController: UIViewController {
     
     @IBAction func convertNumber(_ sender: Any) {
         guard let inputNumber = inputText.text, inputText.text != "" else {
-            inputText.text = "Please enter phone numbers to convert."
+            outputText.text = "Please enter phone numbers to convert."
             return
         }
         outputText.text = ""
+        builtNumberFromLoop = ""
+        
+        for character in inputNumber {
+            switch character {
+            case "A","B","C":
+                let character = 2
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            case "D","E","F":
+                let character = 3
+                 builtNumberFromLoop += String(character)
+                 print(builtNumberFromLoop)
+            case "G","H","I":
+                let character = 4
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            case "J","K","L":
+                let character = 5
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            case "M","N","O":
+                let character = 6
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            case "P","Q","R":
+                let character = 7
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            case "S","T","U":
+                let character = 8
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            case "V","X","Y","Z":
+                let character = 9
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
+            default:
+                print(character)
+                builtNumberFromLoop += String(character)
+                print(builtNumberFromLoop)
 
+            }
         }
+        outputText.text = builtNumberFromLoop
     }
     
     
